4504ff43a5f31fc585f96581b51088690b601d3d
facundo <fmabeldano@dc.uba.ar>
2022-06-23 12:11:39 -0300

    Version 0.4.2 >Fixed urls

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('create_blog/', create_blog_post, name='create.blog')
+    path('blog_posts/', blog_posts, name='blog_posts'),
+    path('blog_post/<blog_post_id>', blog_post, name='blog_post'),
+    path('blog_posts/create/', create_blog_post, name='blog_post_create'),  # ---------------------CRUD [CREATE]
+    path('blog_post/delete/<blog_post_id>', delete_blog_post, name='blog_post_delete'),  # --------CRUD [DELETE]
+    path('blog_post/edit/<blog_post_id>', edit_blog_post, name='blog_post_edit'),  # --------------CRUD [EDIT]
 ]
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,37 +1,15 @@
-"""blog_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from blog_app.views import *
 from accounts_app.views import *
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('about/', about, name='about'),
-    path('blog_posts/', blog_posts, name='blog_posts'),
-    path('blog_post/<blog_post_id>', blog_post, name='blog_post'),
-    # CRUD
-    path('blog_posts/create/', create_blog_post, name='blog_post_create'),
-    path('blog_post/delete/<blog_post_id>',
-         delete_blog_post, name='blog_post_delete'),
-    path('blog_post/edit/<blog_post_id>',
-         edit_blog_post, name='blog_post_edit'),
-    path('blog/', include('blog_app.urls')),
-    path('accounts/', include('accounts_app.urls')),
-]
+try:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', home, name='home'),
+        path('about/', about, name='about'),
+        path('blogs/', include('blog_app.urls')),
+        path('accounts/', include('accounts_app.urls')),
+    ]
 
 handler404 = 'blog_app.views.page_not_found'
