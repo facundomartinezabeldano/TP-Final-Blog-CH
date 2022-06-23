dc3bc17044ae5f34f454bc47972317c42f69a355
facundo <fmabeldano@dc.uba.ar>
2022-06-23 12:12:11 -0300

    Version 0.4.1 >Fixed urls

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -3,13 +3,11 @@ from django.urls import path, include
 from blog_app.views import *
 from accounts_app.views import *
 
-try:
-    urlpatterns = [
-        path('admin/', admin.site.urls),
-        path('', home, name='home'),
-        path('about/', about, name='about'),
-        path('blogs/', include('blog_app.urls')),
-        path('accounts/', include('accounts_app.urls')),
-    ]
-
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('blogs/', include('blog_app.urls')),
+    path('accounts/', include('accounts_app.urls')),
+]
 handler404 = 'blog_app.views.page_not_found'
