8a666ea1b1351ef220b518cf992d82c5ca1432c5
facundo <fmabeldano@dc.uba.ar>
2022-06-26 18:44:21 -0300

    Version 0.4.3.1
    >Quick fix urls (accounts app)

--- accounts_app/urls.py
+++ accounts_app/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', view=home_accounts, name='home_accounts'),
     path('login/', view=login, name='login'),
     path('log-out/', view=LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('sign-up/', view=sign_up, name='sign-up'),
